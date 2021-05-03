cdf2e570d0ac9d1a79e18d0957655e617d4b7a29
Mushira <Mushira.shaikh1999@gmail.com>
2021-05-04 03:14:54 +0530

    First ommit

---
+++ exp1_demo.java
@@ -0,0 +1,11 @@
+class exp1_demo
+{
+    public static void main(String[] args) {
+        System.out.println("Table");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Value of 2*"+i+" = "+(2*i));
+        }
+        
+    }
+    
+}
