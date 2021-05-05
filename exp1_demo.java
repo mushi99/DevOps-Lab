83512afcfbf97eb354d7d1e21007b2935abf05f3
mushi99 <Mushira.shaikh1999@gmail.com>
2021-05-06 04:28:23 +0530

    test

--- exp1_demo.java
+++ exp1_demo.java
@@ -3,7 +3,7 @@ class exp1_demo
     public static void main(String[] args) {
         System.out.println("Table");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Value of 2*"+i+" = "+(2*i));
+            System.out.println("Value of 3*"+i+" = "+(2*i));
         }
         
     }
